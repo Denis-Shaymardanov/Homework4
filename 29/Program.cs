8ce69ca03b1361da7e71f4f7e126af81c5bedf21
Denis Shaymardanov <d9655352866@gmail.com>
2022-07-03 20:51:00 +0500

    added task 29

---
+++ 29/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и 
+//выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+int[] InputArray()
+{
+    int[] result = new int[8];
+    for(int i=0;i<8;i++)
+    {
+        Console.WriteLine("Введите элемент массива");
+        result[i]=Convert.ToInt32(Console.ReadLine());
+    }
+    return result;
+}
+
+void OutputArray(int[] Arr)
+{
+    int N = Arr.Length;
+    for(int i=0;i<N;i++)
+    {
+        if(i==0) Console.Write("[" + Arr[i]);
+        else Console.Write("," + Arr[i]);
+    }
+    Console.Write("]");
+}
+
+OutputArray(InputArray());
